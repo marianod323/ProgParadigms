49b2a8e5d091238272b923bffd88e95a8dfbd3b7
Mariano Dorneles <marianodorneles@gmail.com>
2018-03-14 16:13:24 -0300

    Trabalho 1
    Trabalho 1 da disciplina de Paradigmas de Programação

---
+++ t1/t1.hs
@@ -0,0 +1,32 @@
+-- Crie uma função isVowel :: Char -> Bool que verifique se um caracter é uma vogal ou não.
+
+isVowel :: Char -> Bool
+isVowel c = elem c "aeiouAEIOU"
+
+-- 2) Escreva uma função addComma, que adicione uma vírgula no final de cada string contida numa lista.
+
+addComma :: [String] -> [String]
+addComma list = map (\x -> x ++ ",") list
+
+-- 3) Crie uma função htmlListItems :: [String] -> [String],
+--    que receba uma lista de strings e retorne outra lista contendo as strings
+--    formatadas como itens de lista em HTML.
+
+htmlListItems :: [String] -> [String]
+htmlListItems list = map (\n -> "<LI>" ++ n ++ "</LI>") list
+
+concatenaHtml :: String -> String
+concatenaHtml x = "<LI>" ++ x ++ "</LI>"
+
+htmlListItemsSem :: [String] -> [String]
+htmlListItemsSem list = map (concatenaHtml) list
+
+-- 4) Defina uma função que receba uma string e produza outra retirando as vogais, conforme os exemplos abaixo.
+
+semVogaisSem :: String -> String
+semVogaisSem x = filter (`notElem` "aeiouAEIOU") x
+
+--semVogais :: String -> String
+--semVogais y = filter (\n -> n == "aeiouAEIOU") y
+
+-- 5)
